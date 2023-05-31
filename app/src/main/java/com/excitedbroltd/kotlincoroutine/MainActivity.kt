80830ea0802dd4f2ec09345fdcb0ff5416c6ae78
diptodev <dipankardebnathcse@yahoo.com>
2023-05-31 23:14:06 +0600

    basic setup done

--- app/src/main/java/com/excitedbroltd/kotlincoroutine/MainActivity.kt
+++ app/src/main/java/com/excitedbroltd/kotlincoroutine/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.excitedbroltd.kotlincoroutine
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.excitedbroltd.kotlincoroutine.databinding.ActivityMainBinding
@@ -10,6 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.buttonSubmit.setOnClickListener {
+            for (i in 1..100000) {
+                Log.i("DebugLog", "onCreate: $i")
+            }
 
+
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
