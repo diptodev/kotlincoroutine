adcd1a1af3f02052c3bc209663eda87e2635593c
diptodev <dipankardebnathcse@yahoo.com>
2023-05-31 23:44:32 +0600

    coroutine working

--- app/src/main/java/com/excitedbroltd/kotlincoroutine/MainActivity.kt
+++ app/src/main/java/com/excitedbroltd/kotlincoroutine/MainActivity.kt
@@ -6,19 +6,35 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.excitedbroltd.kotlincoroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        var count = 0
+        CoroutineScope(Dispatchers.IO).launch {
+            count = getCount()
+
+        }
         binding.buttonSubmit.setOnClickListener {
-            for (i in 1..100000) {
-                Log.i("DebugLog", "onCreate: $i")
-            }
-
-
             Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private suspend fun getCount(): Int {
+        var count = 0;
+        coroutineScope {
+            for (i in 1..200000) {
+                count = i
+                Log.d("TAG", "getCount: $i")
+                withContext(Dispatchers.Main){
+                    binding.textView.text = "$count"
+                }
+
+            }
+        }
+        return count
     }
 }
